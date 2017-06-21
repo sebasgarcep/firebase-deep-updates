9ad5889d6f1f0fab1eb7ce853f7624c8172236a3
Sebastian Garrido <sebasgarcep@gmail.com>
2017-06-21 10:42:31 -0500

    feat: Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Firebase Deep Updates
+
+### Motivation
+
+As of this writing, firebase recommends .update() for multi-location transactional commits to the Realtime Database. What is not natural is that the way .update() works is conceptually different when it is passed an object one level deep than when it it several levels deep. At one level deep it sets the properties at the current reference location, not the whole location, but when it is passed an object more than one level deep it doesn't merge the new data into the previous property value, but completely replaces the value. This package is meant to solve this discrepancy.
+
+### Get Started
+
+`firebase-deep-updates` exports a single function that takes any value (scalar, null, array, object or combinations of all of them) and returns a one-level-deep object with slashed keys representing the locations of scalar and null values. For example, in the following:
+
+```
+const firebaseDeepUpdates = require('firebase-deep-updates');
+
+const updates = firebaseDeepUpdates({
+  name: null,
+  foo: {
+    bar: true,
+  },
+});
+
+admin.database().ref().update(updates);
+```
+
+`updates` is equal to:
+
+```
+{
+  'name': null,
+  'foo/bar': true,
+}
+```
---
+++ __tests__/index.spec.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const firebaseDeepUpdates = require('../lib');
+
+describe('firebase deep updates testing suite', () => {
+  it('should not throw an error', () => {
+    expect(() => firebaseDeepUpdates(null)).not.toThrow();
+  });
+
+  it('should return scalar and null values as-is', () => {
+    expect(firebaseDeepUpdates(null)).toBe(null);
+    expect(firebaseDeepUpdates(true)).toBe(true);
+    expect(firebaseDeepUpdates(100)).toBe(100);
+    expect(firebaseDeepUpdates('str')).toBe('str');
+  });
+
+  it('should return objects one level deep without slashed keys as-is', () => {
+    expect(firebaseDeepUpdates({ foo: 'bar' })).toEqual({ foo: 'bar' });
+  });
+
+  it('should return objects one level deep with slashed keys as-is', () => {
+    expect(firebaseDeepUpdates({ 'foo/bar': 'baz' })).toEqual({ 'foo/bar': 'baz' });
+  });
+
+  it('should return objects more than one level deep without slashed keys as one level deep objects with slashed keys', () => {
+    expect(firebaseDeepUpdates({ foo: { bar: 'baz' } })).toEqual({ 'foo/bar': 'baz' });
+  });
+
+  it('should return objects more than one level deep with slashed keys as one level deep objects with slashed keys', () => {
+    expect(firebaseDeepUpdates({ foo: { 'bar/baz': 'maz' } })).toEqual({ 'foo/bar/baz': 'maz' });
+  });
+
+  it('should strip trailing and leading slashes', () => {
+    expect(firebaseDeepUpdates({ '/foo/bar': true })).toEqual({ 'foo/bar': true });
+    expect(firebaseDeepUpdates({ 'foo/bar/': true })).toEqual({ 'foo/bar': true });
+    expect(firebaseDeepUpdates({ '/foo/bar/': true })).toEqual({ 'foo/bar': true });
+    expect(firebaseDeepUpdates({ nested: { '/foo/bar': true } })).toEqual({ 'nested/foo/bar': true });
+    expect(firebaseDeepUpdates({ nested: { 'foo/bar/': true } })).toEqual({ 'nested/foo/bar': true });
+    expect(firebaseDeepUpdates({ nested: { '/foo/bar/': true } })).toEqual({ 'nested/foo/bar': true });
+  });
+
+  it('should combine slashed keys at all levels of the data tree', () => {
+    expect(firebaseDeepUpdates({ 'foo/bar': { 'baz/maz': 'blue' } })).toEqual({ 'foo/bar/baz/maz': 'blue' });
+  });
+});
